65a7ad98cc1eff6e42dce2a9f1119ef9d2c04456
FluffyQuack <git@fluffyquack.com>
2022-04-05 19:36:00 +0200

    Forced CRT filter off and set 200p to 200p.

--- prboom2/src/RT/rt_main.c
+++ prboom2/src/RT/rt_main.c
@@ -208,6 +208,7 @@ static RgExtent2D GetCurrentHWNDSize()
 
 static dboolean IsCRTModeEnabled(rt_settings_renderscale_e renderscale)
 {
+  return 0; //Fluffy: Force CRT filter off
   return renderscale == RT_SETTINGS_RENDERSCALE_320x200;
 }
 
@@ -223,9 +224,10 @@ static RgExtent2D GetScaledResolution(rt_settings_renderscale_e renderscale)
   {
     case RT_SETTINGS_RENDERSCALE_320x200:
     {
-      // double the resolution, to simulate interlacing
-      RgExtent2D original_doom = { 320 * 2, 200 * 2 };
+      //Fluffy: 200p now means 200p
+      RgExtent2D original_doom = { 320, 200 };
       return original_doom;
+      break;
     }
     case RT_SETTINGS_RENDERSCALE_480:   y = 480; break;
     case RT_SETTINGS_RENDERSCALE_600:   y = 600; break;
